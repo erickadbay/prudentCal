a81a2b1f5b4df5e944588b8e18f7a21eabb678bb
erickadbay <erickadbay@gmail.com>
2016-04-03 14:50:17 -0400

    Fixed the font issue

--- db/schema.rb
+++ db/schema.rb
@@ -40,11 +40,6 @@ ActiveRecord::Schema.define(version: 20160323003700) do
 
   add_index "events", ["course_id"], name: "index_events_on_course_id"
 
-  create_table "lectures", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
